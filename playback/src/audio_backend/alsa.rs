b9f909b744a6af908fb73866b8a065c43b94a5ef
sniperrifle2004 <sniperrifle2004+github@gmail.com>
2020-06-22 10:33:02 +0200

    Move buffer clear into write function

--- playback/src/audio_backend/alsa.rs
+++ playback/src/audio_backend/alsa.rs
@@ -87,14 +87,18 @@ impl Open for AlsaSink {
 
 impl AlsaSink {
     fn write_buffer_to_pcm(&mut self) -> io::Result<()> {
-        let pcm = self.pcm.as_mut().ok_or_else(|| {io::Error::new(io::ErrorKind::Other, "write with no pcm")})?;
-        let io = pcm.io_i16().or_else(|err| io::Result::Err(io::Error::new(io::ErrorKind::Other, err)))?;
-        match io.writei(&self.buffer) {
-            Ok(_) => Ok(()),
-            Err(err) => {
-                pcm.try_recover(err, false).or_else(|err| io::Result::Err(io::Error::new(io::ErrorKind::Other, err)))
-            },
-        }
+        let r = (|| {
+            let pcm = self.pcm.as_mut().ok_or_else(|| {io::Error::new(io::ErrorKind::Other, "write with no pcm")})?;
+            let io = pcm.io_i16().or_else(|err| io::Result::Err(io::Error::new(io::ErrorKind::Other, err)))?;
+            match io.writei(&self.buffer) {
+                Ok(_) => Ok(()),
+                Err(err) => {
+                    pcm.try_recover(err, false).or_else(|err| io::Result::Err(io::Error::new(io::ErrorKind::Other, err)))
+                },
+            }
+        })();
+        self.buffer.clear();
+        r
     }
 }
 
@@ -122,18 +126,17 @@ impl Sink for AlsaSink {
     }
 
     fn stop(&mut self) -> io::Result<()> {
-        if self.pcm.is_some() {
-            let r = (|| {
-                let r = self.write_buffer_to_pcm();
-                self.buffer.clear();
-                r?;
-                let pcm = self.pcm.as_ref().ok_or_else(|| {io::Error::new(io::ErrorKind::Other, "stop with no pcm")})?;
-                pcm.drain().or_else(|err| io::Result::Err(io::Error::new(io::ErrorKind::Other, err)))
-            })();
-            self.pcm = None;
-            r
-        } else {
-            Ok(())
+        match  self.pcm {
+            Some(_) => {
+                let r = (|| {
+                    self.write_buffer_to_pcm()?;
+                    let pcm = self.pcm.as_ref().unwrap();
+                    pcm.drain().or_else(|err| io::Result::Err(io::Error::new(io::ErrorKind::Other, err)))
+                })();
+                self.pcm = None;
+                r
+            },
+            None => Ok(()),
         }
     }
 
@@ -148,9 +151,7 @@ impl Sink for AlsaSink {
                 .extend_from_slice(&data[processed_data..processed_data + data_to_buffer]);
             processed_data += data_to_buffer;
             if self.buffer.len() == self.buffer.capacity() {
-                let r = self.write_buffer_to_pcm();
-                self.buffer.clear();
-                r?
+                self.write_buffer_to_pcm()?
             }
         }
 
